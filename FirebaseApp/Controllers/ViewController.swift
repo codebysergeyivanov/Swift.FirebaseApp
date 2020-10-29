acb778592fa768106caeb1d43d2675647207e7ba
Sergei Ivanov <100bb100hands@gmail.com>
2020-10-29 14:01:10 +0700

    init commit

---
+++ FirebaseApp/Controllers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FirebaseApp
+//
+//  Created by Сергей Иванов on 29.10.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
